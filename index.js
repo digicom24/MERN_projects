e6e57e27335c42ebd8652a965afd9add81719260
digicom24 <digitalcompany24@gmail.com>
2021-12-28 09:40:55 +0530

    1st commit

--- index.js
+++ index.js
@@ -2,10 +2,32 @@ const console = require("console");
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/test", {
+    useNewUrlParser: true,
+    keepAlive: true,
+  })
+  .then(() => {
+    console.log("db Connection open!!!");
+  })
+  .catch((err) => {
+    console.log("Error while connecting to db");
+    console.log(err);
+  });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.listen(3000, "localhost", function () {
+app.get("/dogs", (req, res) => {
+  res.send("loloololo");
+});
+
+app.get("", (req, res) => {
+  res.send("3000 loloololo");
+});
+
+app.listen(3000, "localhost", () => {
   console.log("listening on port 3000");
 });
