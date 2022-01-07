e7d7eaf3ed7d2a8c20729b896d90cfc1d8a453e5
digicom24 <digitalcompany24@gmail.com>
2022-01-07 15:12:28 +0530

    yelp_camp project - till lecture no - 414

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const console = require("console");
 const express = require("express");
+const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
@@ -9,6 +10,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -23,6 +25,47 @@ db.once("open", () => {
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const camp = new Campground(req.body.campground);
+  await camp.save();
+  res.redirect(`/campgrounds/${camp._id}`);
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndDelete(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds`);
 });
 
 app.get("/makecampground", async (req, res) => {
