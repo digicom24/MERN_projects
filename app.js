3138c0961f4c3243761d0d506eb25d924d12ae47
digicom24 <digitalcompany24@gmail.com>
2022-01-09 19:34:41 +0530

    yelp_camp project - till lecture no - 426

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
